cabefca59bc5197a1e1fd0c05e637bd8b6ad3139
Dmitry Akentyev <TogusaRusso@gmail.com>
2017-07-31 19:30:21 +0000

    Updated DB settings for Heroku

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-  url: `mongodb://localhost:27017/nightlife`
+  // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+  url:  process.env.MONGOLAB_URI || process.env.MONGODB_URI ||`mongodb://localhost:27017/nightlife`
 
 }
